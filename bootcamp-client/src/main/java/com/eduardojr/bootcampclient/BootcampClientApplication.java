02c0d805f43ece825027d72e7fae04904fc2f2ef
eduardomsj <eduardomsj@gmail.com>
2020-10-04 00:43:23 -0300

    Project created

---
+++ bootcamp-client/src/main/java/com/eduardojr/bootcampclient/BootcampClientApplication.java
@@ -0,0 +1,13 @@
+package com.eduardojr.bootcampclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampClientApplication.class, args);
+	}
+
+}
